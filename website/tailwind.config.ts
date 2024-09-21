2578b2a19bc6d602c6ecb9486f0c3fe4ce582f29
Sigui Kessé Emmanuel <siguici@proton.me>
2024-09-21 23:35:10 +0100

    :label: Add Tailwind config type

--- website/tailwind.config.ts
+++ website/tailwind.config.ts
@@ -1,6 +1,7 @@
 import starlight from '@astrojs/starlight-tailwind';
 import colorwind from 'colorwind';
 
+/** @type {import('tailwindcss').Config} */
 export default {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   plugins: [starlight, colorwind],

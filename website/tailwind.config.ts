7145bb1fb28e74968598e2426d0aef6f63144f9f
Sigui Kessé Emmanuel <siguici@proton.me>
2024-09-21 23:50:28 +0100

    :wrench: Configure dark mode

--- website/tailwind.config.ts
+++ website/tailwind.config.ts
@@ -3,6 +3,7 @@ import colorwind from 'colorwind';
 
 /** @type {import('tailwindcss').Config} */
 export default {
+  darkMode: ['selector', '[data-theme="dark"]'],
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   plugins: [starlight, colorwind],
 };
